14e2f59ddd63cf350fb6393b33bde3be9f9db629
Patrik Gallik <patrik.gallik@gmail.com>
2015-10-18 23:36:54 +0200

    Update search request

--- public/app.js
+++ public/app.js
@@ -1,34 +1,62 @@
 (function($) {
 
-  function search(query) {
-    $.get('/search?q=' + query)
-      .done(draw);
-  }
-
   var list = $('.list');
+
+  var values = {
+    text: '',
+    name1: '',
+    name2: '',
+    dateFrom: '',
+    dateTo: ''
+  };
+
+  function search() {
+    $.ajax('/search', {
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(values)
+    }).done(draw);
+  }
 
   function draw(data) {
     list.empty();
     if (data.length) {
       list.append(data.map(function(item) {
-        return '<li>' + item.highlight.content + '</li>';
+        return (
+        '<li class=\'item\'>' +
+          '<strong>Subject: </strong>' + item._source.subject + '<br>' +
+          '<strong>From: </strong>' + item._source.from + '<br>' +
+          '<strong>To: </strong>' + item._source.to + '<br>' +
+          '<strong>Date: </strong>' + item._source.date + '<br><br>' +
+          '' + (item.highlight ? item.highlight.content : item._source.content) +
+        '</li>');
       }).join(''));
     }
   }
 
   $('[js-text]').on('keyup', function() {
-    search($(this).val());
+    values.text = $(this).val();
+    search();
   });
 
-  $('[js-name-1]').on('keyup', function() {
-
+  $('[js-name-1]').on('keyup', function(e) {
+    values.name1 = $(this).val();
+    search();
   })
 
   $('[js-name-2]').on('keyup', function() {
-
+    values.name2 = $(this).val();
+    search();
   });
 
-  $('[js-datepicker-1]').datepicker();
-  $('[js-datepicker-2]').datepicker();
+  $('[js-datepicker-1]').datepicker().on('changeDate', function(e) {
+    values.dateFrom = e.date;
+    search();
+  });
+
+  $('[js-datepicker-2]').datepicker().on('changeDate', function(e) {
+    values.dateTo = e.date;
+    search();
+  });
 
 }(jQuery));
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var elasticsearch = require('elasticsearch');
+var moment = require('moment');
+var bodyParser = require('body-parser');
 var app = express();
 
 var client = new elasticsearch.Client({
@@ -9,26 +11,56 @@ var client = new elasticsearch.Client({
 
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json({limit: '50mb'}));
+
 app.get('/', function (req, res) {
   res.render('index', {});
 });
 
-app.get('/search', function(req, res) {
-  client.search({
-    index: 'enron',
-    type: 'mail',
-    body: {
-      query: {
-        match: {
-          content: req.query.q
-        },
-      },
-      highlight: {
-        fields : {
-          content: {}
+app.post('/search', function(req, res) {
+
+  console.log(req.body);
+
+  var body = {
+    query: {
+      bool: {
+        should: []
+      }
+    },
+    highlight: {
+      fields : {
+        content: {}
+      }
+    }
+  }
+
+  if (req.body.text) {
+    body.query.bool.should.push({ "match": { "content":  req.body.text }});
+  }
+
+  if (req.body.name1) {
+    body.query.bool.should.push({ "match": { "from":  req.body.name1 }});
+  }
+
+  if (req.body.name2) {
+    body.query.bool.should.push({ "match": { "to":  req.body.name2 }});
+  }
+
+  if (req.body.dateFrom || req.body.dateTo) {
+    body.filter = {
+      range: {
+        date: {
+          gte: req.body.dateFrom ? req.body.dateFrom : null,
+          lte: req.body.dateTo ? req.body.dateTo : null
         }
       }
     }
+  };
+
+  client.search({
+    index: 'enron',
+    type: 'mail',
+    body: body
   }).then(function (resp) {
     res.json(resp.hits.hits.slice(0,10));
   }, function (err) {
